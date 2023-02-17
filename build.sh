c7f7c49dcdb0e5d59e5468c9b68fcb183f1c5988
Yann Thierry-Mieg <Yann.Thierry-Mieg@lip6.fr>
2023-02-17 16:21:41 +0100

    small patches

--- build.sh
+++ build.sh
@@ -8,34 +8,28 @@ mkdir -p usr/local/share/man/man1/
 
 export INSTALL_DIR=$(pwd)/usr/local/
 
-# build
-cd lola/bin
+
+
+cd lola2
+autoreconf -vfi
+./configure --prefix=$INSTALL_DIR
+make
+make install
+cd ..
 
 # kimwitu dependency
-cd kimwitu++-2.3.13/
-./configure --prefix=$INSTALL_DIR
-make && make install
-cd ..
+#cd kimwitu++-2.3.13/
+#./configure --prefix=$INSTALL_DIR
+#make && make install
+# cd ..
  
 cd sara/
 ./configure --prefix=$INSTALL_DIR
 make && make install
 cd ..
- 
-cd lola/
-# don't build doc
-cp ../../../patches/Makefile.am .
-# issues with new LTO enabled compilers
-cp ../../../patches/generalized.c ./src/Formula/LTL/
-autoreconf -vfi
-./configure --prefix=$INSTALL_DIR
-make && make install
-cd ..
 
+mkdir website
+cp usr/local/bin/lola website/
+cp usr/local/bin/sara website/
+  
 
-
-cd lola2
-autoreconf -vfi
-./configure 
-# -L$DEPFOLDER/static-libs -L$DEPFOLDER/lib/ -L$DEPFOLDER/lib64/ -L$ROOTDIR/usr/local/lib/ -llzma 
-make LDFLAGS="-all-static -static-libgcc -static-libstdc++"
